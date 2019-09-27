e2da67789576d327407682ffe46ea14c9d3b7e79
Miles Van Denburg <54696014+MVanDenburg92@users.noreply.github.com>
2019-09-27 13:34:01 -0400

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -4,4 +4,27 @@
 # Description: ENTER A DESCRIIPTION ABOUT WHAT YOUR CODE IS SUPPOSED TO DO HERE.
 
 def old_macdonald(name):
-    pass # replace this line with code
+    name2 = name.capitalize()
+    name2 = []
+    name2 = name.split()
+    name2[0,3.upper()
+    name 3 = name2.join()
+    print(name2)
+    
+old_macdonald(name)
+    ##for i in name2
+        ##if i == 0
+        # replace this line with code
+          
+def old_macdonald(name):
+    print("Enter in a word to capitalize the 1sta and 4th character)
+    index = 0
+    for i in name   
+    if name.islower == True:
+        name2 = name[0].capitalize() + name[1:4] + name[4:5].capitalize()
+        return name2
+    else:
+        print("You done goofed")
+
+print(old_macdonald("timmy"))
+
